6c1f24ce6b43f5d84d7a77ccab669ff23f518902
Gabriel Oliveira <gabrieldanuzoliveira358@gmail.com>
2021-09-19 19:29:52 -0300

    Finalizado

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.entradas}>
+      <TextInput placeholder="Massa"
+      keyboardType="numeric" 
+      style={styles.input}/>
+      <TextInput placeholder="Altura" 
+      keyboardType="numeric"
+      style={styles.input}/>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={() =>{}}><Text style={styles.buttonText}>Calcular</Text>
+      </TouchableOpacity>
+      <Text style={styles.resultado}></Text>
+      <Text style={[styles.resultado, {fontSize: 35}]}></Text>
     </View>
   );
 }
@@ -15,7 +24,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  input: {
+    height: 80,
+    textAlign: 'center',
+    width: "50%",
+    fontSize: 50,
+    marginTop: 24,
+  },
+  entradas: {
+    flexDirection: 'row',
+  },
+  button: {
+    backgroundColor: "#89ffa5"
+  },
+  buttonText: {
+    alignSelf: 'center',
+    padding: 30,
+    fontSize: 25,
+    color: "#6dc4a4",
+    fontWeight: 'bold',
+  },
+  resultado: {
+    alignSelf: 'center',
+    color: "lightgrey",
+    fontSize: 65,
+    padding: 15,
+  }
 });
